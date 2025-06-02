352b15bbe4dc53b4bede5762eba27b8ffccc7713
Guntyoo <Agungprastiyo288>
2025-06-02 09:57:04 +0700

    Tahap 2

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ document.addEventListener('DOMContentLoaded', () =>{
     const taskInput = document.getElementById('task-input');
     const AddTaskBtn = document.getElementById('add-task-btn');
     const taskList = document.getElementById('tasklist');
+    const toDosContainer = document.querySelector('.to-do-container');
+
+    const toggleEmptyState = () => {
+        toDosContainer.style.width = taskList.children.length > 0 ? '100%':'50%';
+    };
+
     const addTask = (event) => {
         event.preventDefault();
         const taskText = taskInput.value.trim();
@@ -16,16 +22,19 @@ document.addEventListener('DOMContentLoaded', () =>{
         <span>${taskText}</span>
         <div class= 'task-btn'>
         <button class = "edit-btn">
-        <i class= 'fas fa-edit'></i></button></>
-        <button class = "trash-btn">
-        <i class= 'fas fa-trash'></i></button></>
+        <i class= 'fas fa-edit'></i></button>
+        <button class = "del-btn">
+        <i class= 'fas fa-trash'></i></button>
         `;
-
-
+        
+        li.querySelector('.del-btn').addEventListener('click', () => {
+            li.remove();
+        });
 
         
         taskList.appendChild(li);
         taskInput.value = "";
+        toggleEmptyState();
     };
 
     AddTaskBtn.addEventListener('click', addTask);
